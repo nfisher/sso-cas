128f058a4e07fb6b16bce50d5ba9ce54c0e4f96b
Nathan Fisher <nfisher@junctionbox.ca>
2011-08-12 02:30:26 -0700

    [NF] Refactored listTimeSheets into method. Post cloned time sheet.

--- src/main/java/ca/junctionbox/sso/TimeApp.java
+++ src/main/java/ca/junctionbox/sso/TimeApp.java
@@ -1,6 +1,11 @@
 package ca.junctionbox.sso;
 
 import java.io.*;
+import java.net.SocketTimeoutException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 import ca.junctionbox.sso.models.RsaAuthentication;
@@ -57,54 +62,75 @@ public class TimeApp {
             ConsoleUI ui = new ConsoleUI(new BufferedReader(new InputStreamReader(System.in)), new BufferedWriter(new OutputStreamWriter(System.out)));
             RsaAuthentication auth = ui.login(config.username());
 
-
             site.config(config);
             site.login(auth.username(), auth.code());
 
-            // TODO: Implement a clone function.
-            Document time_sheets_home = site.navigateTo("time_sheets");
-            Element time = time_sheets_home.getElementById("time");
-            if(time == null) {
-                err.println("Uh-oh, lost track of time...think you might need to re-authenticate.");
-            }
-            Elements rows = time.getElementsByTag("tr");
+            String href = listTimeSheets(site, ui);
 
-            // prints the header and assumes no more than 4 months of timesheets is likely to be cloned
-            for(int i = 0; i < 19; i++) {
-                out.println(i + ") " + rows.get(i).child(0).text() + " / " + rows.get(i).child(2).text() + " / " + rows.get(i).child(3).text());
-            }
-
-            int row_number = ui.readInt("Time sheet to clone (1): ", 1);
-            Document clone = site.navigateTo(rows.get(row_number).child(7).getElementsByTag("a").get(1).attr("href"));
+            Document clone = site.navigateTo(href);
             Element cloned_form = clone.getElementsByTag("form").first();
             HashMap<String, String> cloned_data = new HashMap<String, String>();
-
-            // TODO: Post data from form to action url.
 
             for(Element input : cloned_form.getElementsByTag("input")) {
                 if (input.attr("type").equals("radio")) {
                     if(input.attr("checked").equals("checked")) {
-                        out.println(input.attr("name") + " = " + input.attr("value"));
+                        cloned_data.put(input.attr("name"), input.attr("value"));
+                    }
+                } else if(input.attr("type").equals("submit")) {
+                    if(input.attr("value").endsWith("draft")) {
+                        cloned_data.put(input.attr("name"), input.attr("value"));
                     }
                 } else {
-                    out.println(input.attr("name") + " = " + input.attr("value"));
+                    cloned_data.put(input.attr("name"), input.attr("value"));
                 }
             }
 
             for(Element select : cloned_form.getElementsByTag("select")) {
-                out.println(select.attr("name"));
+                Element selected_option = select.select("option[selected]").first();
+                if(selected_option != null) {
+                    cloned_data.put(select.attr("name"), select.select("option[selected]").first().val());
+                }
             }
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+            calendar.setFirstDayOfWeek(Calendar.MONDAY);
+            DateFormat date_format = new SimpleDateFormat("dd MMM yyyy");
+            String week_ending = date_format.format(calendar.getTime());
 
-            //site.postTo(cloned_form.attr("action"), cloned_data);
-		} catch(FileNotFoundException ex) {
+            cloned_data.put("time_sheet[week_ending_date_string]",  ui.readString("Week Ending (" + week_ending + "): ", week_ending));
+
+            Document save = site.postTo(cloned_form.attr("action"), cloned_data);
+            err.println(save.select("div#flash_notice").text());
+            err.println(save.select("div#errorExplanation"));
+
+		} catch(SocketTimeoutException ex) {
+            err.println("[ERROR] Socket Timeout.");
+            exit(3);
+        } catch(FileNotFoundException ex) {
 			err.println("[ERROR] Unable to open configuration file.");
             exit(2);
 		} catch(IOException ex) {
 			err.println("[ERROR] A connection error occured.");
-			ex.printStackTrace();
             exit(1);
 		}
 
         exit(0);
 	}
+
+    private static String listTimeSheets(CasSite site, ConsoleUI ui) throws IOException {
+        // TODO: Create model of time sheet and separate UI interactions.
+        Document time_sheets_home = site.navigateTo("time_sheets");
+        Element time = time_sheets_home.getElementById("time");
+        if(time == null) {
+            err.println("Uh-oh, lost track of time...think you might need to re-authenticate.");
+            System.exit(1);
+        }
+        Elements rows = time.getElementsByTag("tr");
+
+        // prints the header and assumes no more than 4 months of timesheets is likely to be cloned
+        for(int i = 0; i < 19; i++) {
+            out.println(i + ") " + rows.get(i).child(0).text() + " / " + rows.get(i).child(2).text() + " / " + rows.get(i).child(3).text());
+        }
+        return rows.get(ui.readInt("Time sheet to clone (1): ", 1)).child(7).getElementsByTag("a").get(1).attr("href");
+    }
 }
