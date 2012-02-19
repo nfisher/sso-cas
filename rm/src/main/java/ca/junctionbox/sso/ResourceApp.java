ef4c7fc46aee0576d067441ef6b4226feec4baa9
Nathan Fisher <nfisher@junctionbox.ca>
2012-02-19 21:14:01 +1100

    Read jigsaw CSV export.

--- rm/src/main/java/ca/junctionbox/sso/ResourceApp.java
+++ rm/src/main/java/ca/junctionbox/sso/ResourceApp.java
@@ -2,19 +2,16 @@ package ca.junctionbox.sso;
 
 import ca.junctionbox.sso.models.AppConfig;
 import ca.junctionbox.sso.ui.ConsoleUI;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 
 import static java.lang.System.err;
 import static java.lang.System.exit;
-import static java.lang.System.in;
-
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.select.Elements;
 
 public class ResourceApp {
     public static void main(String [] $args) {
@@ -32,19 +29,40 @@ public class ResourceApp {
                                 .with(site)
                                 .build();
 
+
+            ArrayList<String> employeeNumbers = new ArrayList<String>();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new DataInputStream(new FileInputStream($args[0]))));
+
+            String currentLine;
+            bufferedReader.readLine(); // ignore the first line
+            while((currentLine = bufferedReader.readLine()) != null) {
+                String [] split = currentLine.split(",", 2);
+                if(!split[0].equals("")) employeeNumbers.add(split[0]);
+            }
+
             ui.login();
-            
-            String [] employeeNumbers = { "12609", "10623", "13535", "13358" };
+            ArrayList<String> employeeGender = new ArrayList<String>(employeeNumbers.size());
 
             long start = System.currentTimeMillis();
             for(String employeeNumber : employeeNumbers) {
                 Document person = site.navigateTo("consultants/" + employeeNumber);
                 Elements genderTags = person.select("dd:matchesOwn(^(Male|Female))");
                 Element label = genderTags.first();
-                System.out.println(employeeNumber + "," + label.text());
+                String gender = "Unknown";
+
+                // hackish way to discard trailing characters.
+                if(label.text().startsWith("Male")) {
+                    gender = "Male";
+                } else if(label.text().startsWith("Female")) {
+                    gender = "Female";
+                }
+                
+                employeeGender.add(gender);
+                System.out.println(employeeNumber + "," + gender);
             }
             long diff = (System.currentTimeMillis() - start) / 1000;
             System.out.println("Total Time: " + diff);
+
         } catch(SocketTimeoutException ex) {
             err.println("[ERROR] Socket Timeout.");
             exit(3);
@@ -59,18 +77,4 @@ public class ResourceApp {
 
         exit(0);
 	}
-
-    private String fileFromArgs(String[] $args, String $defaultFilename) {
-        String filename = $defaultFilename;
-
-        if($args.length > 1)
-        {
-            err.println("Usage: java -jar JAR [FILE]");
-            exit(1);
-        } else if($args.length == 1) {
-            filename = $args[0];
-        }
-
-        return filename;
-    }
 }
